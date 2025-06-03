359940d3f0767b98452f5bf99afb325e1919610d
Baranov SI <baranov.si.work@gmail.com>
2025-06-03 09:59:48 +0800

    fix hero section for mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,27 +11,26 @@ const Hero: React.FC = () => {
   const { t } = useTranslation();
 
     return (
-    <section className="flex-col md:flex-row items-center justify-center px-6 md:px-0 py-0 flex bg-white">
+    <section className="flex-col md:flex-row items-center justify-center px-6 md:px-0 py-0 pt-24 md:pt-0 flex bg-white">
     
     <div className="w-40 h-40 md:w-1/2 md:h-full flex items-center justify-center mb-6 md:mb-0 bg-gray-300">
       <img
         src={max}
-        loading="lazy"
         alt="Max photo"
         className="w-40 h-40 md:w-full md:h-full rounded-full md:rounded-none object-cover"
       />
     </div>
 
-    <div className="md:w-1/2 flex flex-col justify-start items-center text-center md:items-start md:text-left md:pl-10 h-full md:h-[80vh] md:py-10 md:pt-24 space-y-8">
+    <div className="md:w-1/2 flex flex-col justify-center items-center text-center md:items-start md:text-left md:pl-10 h-full md:h-[80vh] py-10 space-y-8">
       <h1 className="text-4xl font-bold">{t('hero.title')}</h1>
-      <p className="line-clamp-4 max-h overflow-hidden text-lg text-gray-700 mt-4">{t('hero.descr')}</p>
+      <p className="text-base md:text-lg lg:text-xl text-gray-700 mt-2">{t('hero.descr')}</p>
       <React.Suspense fallback={<div>Loading...</div>}>
         <PopupButton
       
           url="https://calendly.com/baranov_si/30min"
           rootElement={document.getElementById('root')!}
           text={t('interviewModal.interview_btn')}
-          className="self-center bg-blue-600 text-xl text-white font-bold px-10 py-6 rounded-full shadow hover:bg-blue-700 transition"
+          className="self-center bg-blue-600 text-base md:text-xl text-white font-bold px-6 md:px-10 py-4 md:py-6 rounded-full shadow hover:bg-blue-700 transition"
         />
     </React.Suspense>
     </div>
